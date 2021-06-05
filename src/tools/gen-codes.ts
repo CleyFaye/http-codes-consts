c48e6a2c7855a63170af88164bf994ccf65a34df
Gabriel Paul 'Cley Faye' Risterucci <cleyfaye@gmail.com>
2021-06-05 19:32:32 +0200

    Code cleanup

--- src/tools/gen-codes.ts
+++ src/tools/gen-codes.ts
@@ -12,7 +12,8 @@ const CODE_LIST_FILE = "res/http-status-codes-1.csv";
 const OUTPUT_DIRECTORY = "gen";
 const OUTPUT_FILENAME = "codes.ts";
 const OUTPUT_FILEPATH = join(OUTPUT_DIRECTORY, OUTPUT_FILENAME);
-const promiseParse = promisify(parse);
+const promiseParse = promisify<Buffer, Array<Array<string>>>(parse);
+/** Label from source file that must not be put into output */
 const UNUSED_LABELS = [
   "Unassigned",
   "(Unused)",
@@ -23,72 +24,68 @@ interface CodeDefinition {
   code: number;
 }
 
-/**
- * Transform a human-readable label to a const name using underscore
- */
+/** Transform a human-readable label to a const name using underscore */
 const labelToNameUnderscore = (label: string): string => label.replace(
   /[ -]/gu,
   "_",
 )
   .toUpperCase();
 
-/**
- * Transform a human-readable label to a const name using camelcase
- */
+/** Transform a human-readable label to a const name using camelcase */
 const labelToNameCamel = (label: string): string => label.replace(
   /[ -]/gu,
   "",
 );
 
-/**
- * Convert result to a TypeScript string
- */
+/** Convert result to a TypeScript string */
 const transformResToTypescript = (res: Array<CodeDefinition>): string => {
   const codes = res.map(({name, code}) => `"${name}" = ${code}`).join(",\n");
   return `enum HttpCodes {\n${codes}\n}\nexport default HttpCodes;`;
 };
 
-console.log(`Creating output directory "${OUTPUT_DIRECTORY}"`);
-mkdir(OUTPUT_DIRECTORY, {recursive: true})
-  .then(() => {
-    console.log(`Reading codes from ${CODE_LIST_FILE}`);
-    return readFile(CODE_LIST_FILE, "utf8");
-  })
-  .then<Array<Array<string>>>(promiseParse)
-  .then(csvRows => csvRows.slice(1).reduce<Array<CodeDefinition>>(
-    (result, row) => {
-      const [
-        codeStr,
-        label,
-      ] = row;
-      if (!UNUSED_LABELS.includes(label)) {
-        const camelName = labelToNameCamel(label);
-        const underscoreName = labelToNameUnderscore(label);
-        const code = parseInt(codeStr, 10);
+const convertRowsToCodeDefinitions = (
+  csvRows: Array<Array<string>>,
+) => csvRows.slice(1).reduce<Array<CodeDefinition>>(
+  (result, row) => {
+    const [
+      codeStr,
+      label,
+    ] = row;
+    if (!UNUSED_LABELS.includes(label)) {
+      const camelName = labelToNameCamel(label);
+      const underscoreName = labelToNameUnderscore(label);
+      const code = parseInt(codeStr, 10);
+      result.push(
+        {
+          name: camelName,
+          code,
+        },
+      );
+      if (camelName !== underscoreName) {
         result.push(
           {
-            name: camelName,
+            name: underscoreName,
             code,
           },
         );
-        if (camelName !== underscoreName) {
-          result.push(
-            {
-              name: underscoreName,
-              code,
-            },
-          );
-        }
       }
-      return result;
-    },
-    [],
-  ))
-  .then(res => {
-    const outputContent = transformResToTypescript(res);
-    console.log(`Writting output into "${OUTPUT_FILEPATH}"`);
-    return writeFile(OUTPUT_FILEPATH, outputContent);
-  })
-  .catch(error => {
-    console.error(error);
-  });
+    }
+    return result;
+  },
+  [],
+);
+
+const main = async () => {
+  console.log(`Creating output directory "${OUTPUT_DIRECTORY}"`);
+  await mkdir(OUTPUT_DIRECTORY, {recursive: true});
+  console.log(`Reading codes from ${CODE_LIST_FILE}`);
+  const fileContent = await readFile(CODE_LIST_FILE);
+  const csvRows = await promiseParse(fileContent);
+  const codeDefinitions = convertRowsToCodeDefinitions(csvRows);
+  const outputContent = transformResToTypescript(codeDefinitions);
+  console.log(`Writting output into "${OUTPUT_FILEPATH}"`);
+  await writeFile(OUTPUT_FILEPATH, outputContent);
+  console.log("Done");
+};
+
+main().catch(console.error);
